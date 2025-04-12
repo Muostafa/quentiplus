6f136faf5f439094d92299f3de3ef8c02c6470f7
Mustafa omran <muostafa12@gmail.com>
2025-04-12 08:21:49 +0200

    fix build 2

--- packages/inngest/common/integrations.ts
+++ packages/inngest/common/integrations.ts
@@ -1,4 +1,5 @@
 export const importIntegration = async (path: string) => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-  return await import(`../../integrations/${path}`);
+  // Assuming the build setup resolves workspace packages correctly for dynamic imports
+  return await import(`@quenti/integrations/${path}`);
 };
--- packages/trpc/console.ts
+++ packages/trpc/console.ts
@@ -1,6 +1,7 @@
 export const importConsole = async (path: string) => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-  return (await import(`../console/${path}`)) as {
+  // Assuming the build setup resolves workspace packages correctly for dynamic imports
+  return (await import(`@quenti/console/${path}`)) as {
     usernameAvailable: (username: string) => boolean;
   };
 };
